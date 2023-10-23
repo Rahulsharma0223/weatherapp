5b76e040029cafe5773ee7523b64f12a976c648a
Rahul sharma <120250301+Rahulsharma0223@users.noreply.github.com>
2023-10-24 00:35:25 +0530

    added other icons (rain, drizzle)

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 
 import search_icon from "../Assets/search.png";
@@ -13,6 +13,8 @@ import humidity_icon from "../Assets/humidity.png";
 const WeatherApp = () => {
 
     let api_key ="a85ca238a9ce16ecb87f57bfdcbfe45f";
+
+    const [wicon,setWicon] = useState(cloud_icon);
 
     const search = async () => {
         const element = document.getElementsByClassName("cityInput")
@@ -33,6 +35,39 @@ const WeatherApp = () => {
         wind[0].innerHTML = data.wind.speed+" km/h";
         temprature[0].innerHTML = data.main.temp+"°C";
         location[0].innerHTML = data.name;
+
+        if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n")
+        {
+            setWicon(clear_icon);
+        }
+        else if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n")
+        {
+            setWicon(cloud_icon);
+        }
+        else if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n")
+        {
+            setWicon(drizzle_icon);
+        }
+        else if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n")
+        {
+            setWicon(drizzle_icon);
+        }
+        else if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n")
+        {
+            setWicon(rain_icon);
+        }
+        else if(data.weather[0].icon==="10d" || data.weather[0].icon==="10n")
+        {
+            setWicon(rain_icon);
+        }
+        else if(data.weather[0].icon==="13d" || data.weather[0].icon==="13n")
+        {
+            setWicon(snow_icon);
+        }
+        else
+        {
+            setWicon(clear_icon);
+        }
     }   
   return (
     <div className='container'>
@@ -43,7 +78,7 @@ const WeatherApp = () => {
             </div>
         </div>
         <div className="weather-image">
-            <img src={cloud_icon} alt="" />
+            <img src={wicon} alt="" />
         </div>
         <div className="weather-temp">24°C</div>
         <div className="weather-location">London</div>
